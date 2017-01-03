152b6fb2f8f959549f1ecaf90dd3d0df3baa7cfe
BigGarik <inetsmol@gmail.com>
2017-01-03 13:33:29 +0300

    solution level10.lesson11.bonus03

--- src/com/javarush/test/level10/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level10/lesson11/bonus03/Solution.java
@@ -36,6 +36,17 @@ public class Solution
     public static void sort(int[] array)
     {
         //напишите тут ваш код
-
+        int temp;
+        for (int i = 0; i < array.length-1; i++)
+        {
+            for (int j = i + 1; j < array.length; j++)
+            {
+                if (array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
